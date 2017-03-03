aadd35eb6d23eb48011ea9b5b147b18e9caa5b62
Sphere <syntax1921king@gmail.com>
2017-03-04 05:19:37 +0800

    Add root directory

---
+++ app/src/main/java/com/chronos/sphere/arduinobluetoothvoicecom/ControlActivity.java
@@ -0,0 +1,13 @@
+package com.chronos.sphere.arduinobluetoothvoicecom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control);
+    }
+}
